b15dffa1421f9a6137d8d7f8a3cebd37a3e3904d
Nikolas Karinja <nikobikoliko@gmail.com>
2017-05-06 15:25:17 -0700

    Add files via upload

---
+++ Avix 486 Diskette/486.bat
@@ -0,0 +1,13 @@
+@echo off
+cls
+echo.
+echo Checking Current Operating System...
+echo.
+ver
+echo.
+echo Unzipping Package...
+echo.
+pkunzip -e system.zip
+echo.
+echo Avix 486 has been installed!
+echo. 
